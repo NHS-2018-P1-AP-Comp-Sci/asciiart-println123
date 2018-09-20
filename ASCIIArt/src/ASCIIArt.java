462141dabcf58e38c07d2dd65a5a66312afd6ba8
s-yoonji <s-yoonji@53S500038057.it.bsd405.org>
2018-09-20 08:15:21 -0700

    Mr. Rasmussen, this is Connor and this is my ASCIIArt Project.

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -10,8 +10,54 @@
  * For those of you who have dabbled in coding before, try to contain your code to 
  * just what we have discussed in this unit.  
  * 
- * @author Mr. Rasmussen 
+ * @author Mr. Raw
  *
  */
 
 // YOUR CODE GOES HERE :)
+public class ASCIIArt {
+	public static void C () {
+		System.out.println("   _____");
+		System.out.println(" /  ____|");
+		System.out.println(" |  |");
+		System.out.println(" |  |");
+		System.out.println(" |  |___");
+		System.out.println(" \\______|");
+	}
+	public static void O () {
+		System.out.println(" ______ ");
+		System.out.println("/  __  \\");
+		System.out.println("| |  | |");
+		System.out.println("| |  | |");
+		System.out.println("| |__| |");
+		System.out.println("\\______/");
+	}
+	public static void N () {
+		System.out.println("_____   __  ");
+		System.out.println("|   \\  | |");
+		System.out.println("|   _\\ | |");
+		System.out.println("| | \\ \\| |");
+		System.out.println("| |  \\   |");
+		System.out.println("|_|   \\__|");
+	}
+	public static void R () {
+		System.out.println("______");
+		System.out.println("|  __  \\         ");
+		System.out.println("| |__| /         ");
+		System.out.println("|  ___ \\  ");
+		System.out.println("|  | \\  \\ ");
+		System.out.println("|__|  \\__|");
+	}
+	public static void main(String[] args) {
+	C ();
+	O ();
+	N ();
+	N ();
+	O ();
+	R ();
+	System.out.println("\n");
+	System.out.println("This is my ASCIIArt Project\n");
+	System.out.println("Connor Loves Computer Science!!!!!!");
+	
+}
+}
